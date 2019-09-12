bbf02e26ab08631690d4589d1c0b9da638fb9269
Mark Marron <marron@microsoft.com>
2019-09-12 15:59:36 -0700

    aot representation

--- ref_impl/src/aot/runtime/value.h
+++ ref_impl/src/aot/runtime/value.h
@@ -3,53 +3,121 @@
 // Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 //-------------------------------------------------------------------------------------------------------
 
+#include <cstdlib>
 #include <cstdint>
+
+#define NOT_IMPLEMENTED(OP) (BSQ::fail(OP, __FILE__, __LINE__))
+
+#define RUNTIME_ERROR(MSG) (BSQ::fail(OP, __FILE__, __LINE__))
+#define BSQ_ABORT(MSG) (BSQ::fail(OP, __FILE__, __LINE__))
 
 namespace BSQ
 {
-enum class Tag
-{
-    //none is the special nullptr value
-    ref_tag = 0x0, 
-    true_tag = 0x1,
-    false_tag = 0x2,
-    inlineint_tag = 0x4
-};
+    void fail(const char* msg, const char* file, int64_t line, ...)
+    {
+        exit(1);
+    }
 
-typedef int32_t inline_int;
-typedef void* Value;
+    typedef void* Value;
 
-constexpr Value bnone = (Value)0x0;
-constexpr Value btrue = (Value)0x1;
-constexpr Value bfalse = (Value)0x2;
+    constexpr int32_t  VarTag_Shift       = 48;
+    constexpr int32_t  VarTagInline_Shift = 46;
 
-inline bool JavascriptNumber::Is_NoTaggedIntCheck(Var aValue)
-{
-    return ((uint64)aValue >> 50) != 0;
-}
+    constexpr uint64_t FloatTag_Value = (((uint64_t)0xFFFCull) << VarTag_Shift);
+    constexpr uint64_t AtomTag_Value  = (((uint64_t)0x1i64) << VarTag_Shift);
+    
+    constexpr uint64_t TrueTagInline_Value  = (((uint64_t)0x1i64) << VarTagInline_Shift);
+    constexpr uint64_t FalseTagInline_Value = (((uint64_t)0x2i64) << VarTagInline_Shift);
 
-inline double JavascriptNumber::GetValue(Var aValue)
-{
-    AssertMsg(Is(aValue), "Ensure var is actually a 'JavascriptNumber'");
-    double result;
-    (*(uint64 *)(&result)) = (((uint64)aValue) ^ FloatTag_Value);
-    return result;
-}
+    constexpr uint64_t ReferenceTag_Mask = (FloatTag_Value | AtomTag_Value);
+    constexpr uint64_t TagInline_Mask    = (((uint64_t)0x3i64) << VarTagInline_Shift);
 
-inline Tag getTagFrom(Value v)
-{
-    return (Tag)(((uint64_t)v) & 0xF);
-}
+    constexpr Value NoneInline_Value  = (Value)((uint64_t)0x0);
+    constexpr Value TrueInline_Value  = (Value)(AtomTag_Value | TrueTagInline_Value);
+    constexpr Value FalseInline_Value = (Value)(AtomTag_Value | FalseTagInline_Value);
 
-inline inline_int unboxInlineInt(Value v)
-{
-    return (inline_int)(((int64_t)v) >> 4);
-}
-inline Value boxInlineInt(inline_int i)
-{
-    return (Value)(((uint64_t)(i << 4)) & ((uint64_t)Tag::inlineint_tag));
-}
+    constexpr uint64_t k_Nan = 0xFFF8000000000000ull;
 
+    inline uint64_t doubleConvert(double value)
+    {
+        return  *((uint64_t*)(&value));
+    }
 
+    inline bool IsNan(double value)
+    {
+        uint64_t nCompare = doubleConvert(value);
+        return ((~nCompare & 0x7FF0000000000000ull) == 0) & ((nCompare & 0x000FFFFFFFFFFFFFull) != 0);
+    }
+
+    inline bool isNone(Value v)
+    {
+        return v == NoneInline_Value;
+    }
+
+    inline bool isTrue(Value v)
+    {
+        return v == TrueInline_Value;
+    }
+
+    inline bool isFalse(Value v)
+    {
+        return v == FalseInline_Value;
+    }
+
+    inline bool isInlineAtom(Value v)
+    {
+        return (((uint64_t)v) & AtomTag_Value) != 0;
+    }
+
+    inline bool isInlineFloat(Value v)
+    {
+        return (((uint64_t)v) & FloatTag_Value) != 0;
+    }
+
+    inline bool isReference(Value v)
+    {
+        return (v != NoneInline_Value) & (((uint64_t)v & ReferenceTag_Mask) == 0);
+    }
+
+    inline bool isBool(Value v)
+    {
+        return isInlineAtom(v) & ((((uint64_t)v) & TagInline_Mask) != 0);
+    }
+
+    inline bool isInt(Value v)
+    {
+        return isInlineAtom(v) & (((uint64_t)v) & TagInline_Mask) == 0;
+    }
+
+    inline Value boolToVar(bool value)
+    {
+        return value ? TrueInline_Value : FalseInline_Value;
+    }
+
+    inline Value smallintToVar(int32_t value)
+    {
+        return (Value*)(((uintptr_t)(uint32_t)value) | AtomTag_Value);
+    }
+
+    inline Value intToVar(int64_t value)
+    {
+        if((value < INT32_MIN) || (value > INT32_MAX))
+        {
+            return (Value*)(((uintptr_t)(uint32_t)value) | AtomTag_Value);
+        }
+
+        NOT_IMPLEMENTED("intToVar -- BigInt");
+    }
+
+    inline Value floatToVar(double value)
+    {
+         if (IsNan(value))
+        {
+            value = *((double*)(&k_Nan));
+        }
+
+        uint64_t val = doubleConvert(value);
+        return (Value)(val ^ FloatTag_Value);
+    }
 
 } // namespace BSQ
