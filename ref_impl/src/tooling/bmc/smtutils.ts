7fd12fa5284b45a6cff1547485982b8fcb1fffae
Mark Marron <marron@microsoft.com>
2019-10-02 12:39:47 -0700

    added files

---
+++ ref_impl/src/tooling/bmc/smtutils.ts
@@ -0,0 +1,29 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
+function sanitizeForSMT(name: string): string {
+    return name
+    .replace(/#/g, "$h$")
+    .replace(/::/g, "$cc$")
+    .replace(/=/g, "$eq$")
+    .replace(/\[/g, "$lb$")
+    .replace(/\]/g, "$rb$")
+    .replace(/{/g, "$lc$")
+    .replace(/}/g, "$rc$")
+    .replace(/</g, "$la$")
+    .replace(/>/g, "$ra$")
+    .replace(/\|/g, "$v$")
+    .replace(/--/g, "$dd$")
+    .replace(/, /g, "$csp$")
+    .replace(/[.]/g, "$dot$")
+    .replace(/:/g, "$c$")
+    .replace(/[\\]/g, "$bs$")
+    .replace(/[/]/g, "$fs$")
+    .replace(/%/g, "$pct$");
+}
+
+export {
+    sanitizeForSMT
+};
